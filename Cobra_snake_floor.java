8d9d319e74ec7af58e7fad4156b98988d0fcd94b
Shivansh20128 <88429611+Shivansh20128@users.noreply.github.com>
2021-10-29 22:29:12 +0530

    Add files via upload

--- Cobra_snake_floor.java
+++ Cobra_snake_floor.java
@@ -3,9 +3,12 @@ package com.brickBracker;
 public class Cobra_snake_floor extends Floor{
     public Cobra_snake_floor(){ }
     private Player my_player;
+    private int floor_no;
 
     public void cobra_floor(Player p, Floor f){
         my_player=p;
+        floor_no=p.getFloor_no();
+        f.set_floor_no(floor_no);
         System.out.println(my_player.name + " has reached king cobra");
         my_player.setScore(my_player.getScore()-4);
         System.out.println("Total points: " + my_player.getScore());
