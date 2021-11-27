66f6b37c752ee87c6df21137714348ebe8ce474b
Shivansh20128 <88429611+Shivansh20128@users.noreply.github.com>
2021-11-27 16:56:32 +0530

    Delete User.java

--- User.java
+++
@@ -1,9 +0,0 @@
-package com.brickBracker;
-
-public interface User {
-    int id=0;
-    void View_lectures();
-    void View_assessments();
-    void View_comments(Student x, Student y, Student z, Instructor a, Instructor b);
-    void add_comments();
-}
